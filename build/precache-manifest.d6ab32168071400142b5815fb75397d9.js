fd25e586e857c11584c6c869e6f8ccf1c13e51ad
roy collins nguithi <roynguithi@student.moringaschool.com>
2024-08-04 22:05:05 +0300

    react-hooks-import-export-lab

---
+++ build/precache-manifest.d6ab32168071400142b5815fb75397d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "321fceaef796626ad2e967b7b48bb393",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9bb3ce9134603395599",
+    "url": "/static/css/main.ca2d9c4d.chunk.css"
+  },
+  {
+    "revision": "509bffc3b1d1091d32bc",
+    "url": "/static/js/2.67af74b8.chunk.js"
+  },
+  {
+    "revision": "d9bb3ce9134603395599",
+    "url": "/static/js/main.74755912.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { username, city } from "../data/user";
 
 function Home() {
   return (
@@ -9,3 +10,23 @@ function Home() {
     </div>
   );
 }
+
+function UserDetails() {
+  return (
+    <div>
+      <h1>Username: {username}</h1>
+    </div>
+  );
+}
+
+function Location() {
+  return (
+    <div>
+      <h1>City: {city}</h1>
+    </div>
+  );
+}
+
+// Export default and named exports
+export default Home;
+export { UserDetails, Location };
